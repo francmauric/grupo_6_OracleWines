55ffc4b7c182f5e2b8b1f0e7f1feab3efb985b65
francmauric <franc_mauric1@hotmail.com>
2022-11-17 10:34:17 +0000

    cambio claver y username de planetscale

--- database/config/config.js
+++ database/config/config.js
@@ -16,9 +16,9 @@ module.exports = {
     ssl: { rejectUnauthorized: true },
   },
   production: { /* agrega la config de planet scale (user, pass y database) */
-    username: "o4dmow8hwejhnawjydmk", 
-    password: "pscale_pw_3s8Ttkoy9JRemmpsD6FRMMbUXtcISrJbN3vtENd11Qh",
-    database: "sport_shoes",
+    username: "ie8by5iktvowamgb3f7c", 
+    password: "pscale_pw_5KA6pTwX9u4kppLKfqiGdWPrOVJkDDzaz8pQ8j1pvGI",
+    database: "structure",
     host: "us-east.connect.psdb.cloud",
     dialect: "mysql",
     dialectOptions: {
